2664df7e764032f6c09c225afd2ad12662f67188
AThinh <macbookairm2@192.168.1.6.non-exists.ptr.local>
2025-05-10 23:52:03 +0700

    10-05

--- module 2/Java/day-of-month/src/DayOfMonth.java
+++ module 2/Java/day-of-month/src/DayOfMonth.java
@@ -7,9 +7,10 @@ public class DayOfMonth {
         System.out.print("Which month that you want to count days? ");
         int month = scanner.nextInt();
 
+        String daysInMonth;
         switch (month) {
             case 2:
-                System.out.print("The month '2' has 28 or 29 days!");
+                daysInMonth = "28 or 29";
                 break;
             case 1:
             case 3:
@@ -18,8 +19,19 @@ public class DayOfMonth {
             case 8:
             case 10:
             case 12:
-                System.out.print("The month '" + month + "' has 31 days!");
+                daysInMonth = "31";
                 break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                daysInMonth = "30";
+                break;
+            default:
+                daysInMonth = "";
         }
+
+        if (!daysInMonth.equals("")) System.out.printf("The month '%d' has %s days!", month, daysInMonth);
+        else System.out.print("Invalid input!");
     }
 }
