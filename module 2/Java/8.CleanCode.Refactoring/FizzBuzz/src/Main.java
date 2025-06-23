bbccc511abea289a80b26654e0b7153d9a638424
AThinh <macbookairm2@MacBook-Air-cua-MACBOOK.local>
2025-06-23 16:38:58 +0700

    23-06

---
+++ module 2/Java/8.CleanCode.Refactoring/FizzBuzz/src/Main.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        String result = FizzBuzz.fizzBuzz(5);
+        System.out.println("Kết quả: " + result);
+    }
+}
