02a7080dd87271a6c46c3c3064bc9f651c18d77f
AThinh <macbookairm2@Mac-mini-cua-Du.local>
2025-11-17 08:12:38 +0700

    Add demo l18n folder

---
+++ Module 4/BaiTap/demo l18n/src/main/java/com/codegym/demo_l18n/DemoL18nApplication.java
@@ -0,0 +1,13 @@
+package com.codegym.demo_l18n;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoL18nApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoL18nApplication.class, args);
+    }
+
+}
---
+++ Module 4/BaiTap/demo l18n/src/test/java/com/codegym/demo_l18n/DemoL18nApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym.demo_l18n;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoL18nApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
